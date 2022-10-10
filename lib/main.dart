975e8c6091b0730c2b78ba0dd11305653aa089b9
Ahmed <ahmad_bhai28@yahoo.com>
2022-10-10 21:47:10 +0200

    added seven working colors with notes

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,72 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = new AudioCache();
-                player.play("note1.wav");
-              },
-              child: Text("Click me !!!"),
-            ),
+          child: Column(
+            children: [
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red)),
+                onPressed: () {
+                  final player = new AudioCache();
+                  player.play("note1.wav");
+                },
+                child: Text(""),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                onPressed: () {
+                  final player = new AudioCache();
+                  player.play("note2.wav");
+                },
+                child: Text(""),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green)),
+                onPressed: () {
+                  final player = new AudioCache();
+                  player.play("note3.wav");
+                },
+                child: Text(""),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.yellow)),
+                onPressed: () {
+                  final player = new AudioCache();
+                  player.play("note4.wav");
+                },
+                child: Text(""),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.purple)),
+                onPressed: () {
+                  final player = new AudioCache();
+                  player.play("note5.wav");
+                },
+                child: Text(""),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.pink)),
+                onPressed: () {
+                  final player = new AudioCache();
+                  player.play("note6.wav");
+                },
+                child: Text(""),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.orange)),
+                onPressed: () {
+                  final player = new AudioCache();
+                  player.play("note7.wav");
+                },
+                child: Text(""),
+              ),
+            ],
           ),
         ),
       ),
